c3ef92f4bca3459e99ab9b65914531c5b38b2dfb
Theo <13920881+theloni-monk@users.noreply.github.com>
2021-03-14 23:13:34 -0700

    fixed zindex bug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import {Box, Heading} from '@primer/components'
 const Footer = () => {
-    return (<Box bg="#101818" width="100%" height="auto" color='grey'>
+    return (<Box bg="#101818" width="100%" height="auto" color='grey' sx={{position:'relative', zIndex:'1!important'}}>
       <Heading p='3%' pb='0' textAlign='left' fontSize='medium'>Copyright © Thelonious Cooper 2021</Heading>
       <Heading p='3%' pt='1%' pb='2%' textAlign='left' fontSize='medium'>I made this website from scratch! Check it out <a style={{ color: 'grey' }} href='https://github.com/theloni-monk/whoami'>here</a></Heading>
   
