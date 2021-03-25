f47df435ec9b8a88f84c21e041c5266647eb35cb
Theo <13920881+theloni-monk@users.noreply.github.com>
2021-03-25 00:27:14 -0700

    misc

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,16 +1,22 @@
-import {Box, Heading} from '@primer/components'
-const Intro = (props: { headerOffset: number }) => {
-    return (
-      <Box
-        sx={{ minHeight: "100px", position: 'relative', zIndex: 0 }}
-        padding={"10%"}
-        mt={props.headerOffset + "px"}
-        mb={0}
-      >
-        <Heading fontStyle="italic" color="white" fontSize="xxx-large" sx={{ position: 'relative', zIndex: 3 }}>
-          Hi, my name is Theo
+import { Box, Heading } from '@primer/components'
+import {useState, useLayoutEffect} from 'react'
+const Intro = (props: { headerOffset: number, bodHeight: number }) => {
+  //TODO: fixed heading pos and bigger font size
+  //TODO: make bigger
+ 
+  //TODO: add bouncing down arrow
+  return (
+    <Box
+      sx={{  position: 'relative', zIndex: 0 }}
+      padding={"10%"}
+      mt={props.headerOffset + "px"}
+      mb={0}
+      minHeight={props.bodHeight - props.headerOffset}
+    >
+      <Heading fontStyle="italic" color="white" fontSize="10vw" sx={{ position: 'relative', zIndex: 3,textShadow: '2px 2px 8px #000000' }}>
+        Hi, my name is Theo
         </Heading>
-      </Box>
-    );
-  };
+    </Box>
+  );
+};
 export default Intro;
--- src/components/ProjectWidget.tsx
+++ src/components/ProjectWidget.tsx
@@ -1,33 +1,58 @@
 //stole this from ian lol
-//TODO: steal ian's css
+import { Heading,Grid, Box } from '@primer/components'
+//TODO: Make this look decetn
 interface ProjectProps {
-    name: string;
-    image?: string;
-    isLogo?: boolean;
-    description: string;
-    href: string;
-  }
+  name: string;
+  image?: string;
+  isLogo?: boolean;
+  description: string;
+  href: string;
+} 
 const ProjectWidget = (props: ProjectProps) => {
-    const makeImage = () => {
-      if (props.image) {
-        return (
-          <div className="project-image">
-            <img src={props.image} alt='name'></img>
-            {!props.isLogo ? <div className="overlay"></div> : null}
-          </div>
-        );
-      } else {
-        return null;
-      }
-    }
-    return (
-      <div className="project" onClick={() => {
-        window.location.href = props.href;
+  
+  return (
+    <div className="project" onClick={() => {
+      window.location.href = props.href;
+    }}
+      style={{
+        backgroundColor:'#242424',
+        color:'white',
+        width:'100%',
+        paddingLeft: '2%',
+        boxShadow: ' 0px 2px 10px #0000002a',
+        borderRadius: '10px',
+        position: 'relative',
+        display: 'flex',
+        overflow: 'hidden',
+        margin:'5%',
+        maxHeight: '200px'
       }}>
-        <h1>{props.name}</h1>
-        {makeImage()}
-        <span className="description">{props.description}</span>
-      </div>
-    );
-  }
+      <Grid gridTemplateColumns='repeat(1,auto)'gridGap='0' p='5%'>
+      <Heading>{props.name}</Heading>
+      
+      <div className="description" style={{ display: 'block', marginTop: '10px' }} >{props.description}</div>
+      </Grid>
+      <Box width='100%' mr = '0' ml='3%'>
+        <img  min-height='100%' max-width='200%' src={process.env.PUBLIC_URL+'/res/'+ props.image} alt={props.name} />
+      </Box>
+    </div>
+  );
+}
 export default ProjectWidget;
+/**
+ * .project {
+    height: 100px;
+    margin: 40px;
+    @extend %box;
+
+    h1 {
+        margin: 0;
+        font-size: 18pt;
+    }
+
+
+    transition: 0.3s;
+    user-select: none;
+    cursor: pointer;
+}
+ */
