d0642dd70a2c5664f496cf7040271b47da21e4e1
Theo <theo.acooper@gmail.com>
2022-11-25 08:00:29 -0800

    escaped apostrophe character

--- components/Greeter.tsx
+++ components/Greeter.tsx
@@ -15,7 +15,7 @@ const Greeter = (props: { headerOffset: number, bodHeight: number, isMobile:bool
         Hi, my name is <br/> Thelonious. 
         </Heading>
         <Heading className='animate__animated animate__fadeIn' fontStyle="italic" color="white" fontSize={props.isMobile? "5vh":"5vw"} sx={{ position: 'relative', zIndex: 3,textShadow: '2px 2px 8px #000000' }}>
-        I'm a 2nd year student at MIT EECS.
+        I&apos;m a 2nd year student at MIT EECS.
         </Heading>
       <StyledOcticon className='animate__animated animate__pulse animate__infinite'  size='large' color='white' icon={ChevronDownIcon} sx={{ position:'absolute', bottom:'5%', zIndex:3, justifySelf:'center'}}/>
     </Box>
